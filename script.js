69dd8e667200838ab18e75fc03352c7847b9ab00
marcosefgomes <m4rc05.g0m35@gmail.com>
2023-08-23 22:05:51 -0400

    music

--- script.js
+++ script.js
@@ -4,24 +4,41 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const title = document.querySelector(".app__title");
-const titleStrong = document.querySelector(".app__title-strong");
+const buttons = document.querySelectorAll(".app__card-button");
+const musicFocusInput = document.querySelector("#alternar-musica");
+const music = new Audio("/sons/luna-rise-part-one.mp3");
+music.loop = true;
+
+musicFocusInput.addEventListener("change", () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
 
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
+  focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
   alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
   alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
 });
 
-function alterarContexto(contexto) {
-  html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `/imagens/${contexto}.png`);
-  switch (contexto) {
+function alterarContexto(context) {
+  buttons.forEach(function (context) {
+    context.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", context);
+  banner.setAttribute("src", `/imagens/${context}.png`);
+  switch (context) {
     case "foco":
       title.innerHTML = `
       Otimize sua produtividade,<br>
